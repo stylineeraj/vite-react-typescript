583d17da6cfa856223eb2877a6f35223e9a2d0df
stylineeraj <neeraj@stylishop.com>
2026-02-10 16:33:35 +0530

    add pre commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,11 @@ export function App() {
   return (
     <div className='flex items-center justify-center min-h-screen'>
       <div className='font-medium'>Hello World</div>
-      <div className='font-medium'>User: {   user.name}</div>
-      <div className='font-medium'>Count: {   count}</div>
-      <button onClick={increment}>Increment
-
-      </button>
-      <button onClick={decrement}>Decrement
-
-      </button>
-      <button onClick={reset}>Reset
-        
-      </button>
+      <div className='font-medium'>User: {user.name}</div>
+      <div className='font-medium'>Count: {count}</div>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
       <button onClick={() => incrementBy(10)}>Increment By 10</button>
     </div>
   );
